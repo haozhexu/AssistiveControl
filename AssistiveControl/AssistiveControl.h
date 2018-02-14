372191708a17b679e8a6c650780f3fbe23de8748
Haozhe XU <huiimystudio@gmail.com>
2018-02-15 05:01:26 +1100

    Initial Commit

---
+++ AssistiveControl/AssistiveControl.h
@@ -0,0 +1,19 @@
+//
+//  AssistiveControl.h
+//  AssistiveControl
+//
+//  Created by Haozhe XU on 15/2/18.
+//  Copyright © 2018 Haozhe XU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AssistiveControl.
+FOUNDATION_EXPORT double AssistiveControlVersionNumber;
+
+//! Project version string for AssistiveControl.
+FOUNDATION_EXPORT const unsigned char AssistiveControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssistiveControl/PublicHeader.h>
+
+
